c71dc5cc3084c32eb5056d3b9b8b14128ec3bae2
Filippo Aleotti <filippoaleotti@yahoo.it>
2020-02-26 09:59:40 +0100

    Update generate_proxy.sh

--- generate_proxy.sh
+++ generate_proxy.sh
@@ -1,16 +1,16 @@
 #!bin/bash
 k="/media/faleotti/SSD/FULL_KITTI"
 proxy_path=
-depth_hints_path=
+kitti_path=
 training='ours'
 if [ $training == 'ours' ]; then
     # ours
-    python precompute_depth_hints.py --data_path $k
+    python precompute_depth_hints.py --data_path $proxy_path
     --save_path  './proxies/ours' 
 
 else
     # original
-    python precompute_depth_hints.py --data_path $k \
+    python precompute_depth_hints.py --data_path $kitti_path \
     --save_path './proxies/original' \
     --compute
   fi
